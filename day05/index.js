92ed04764c9feff377c07af5f03ca2d8a44cf04f
Tobias Andersson <HT86@live.se>
2023-12-04 15:24:01 +0100

    day5 prep

---
+++ day05/index.js
@@ -0,0 +1,31 @@
+const fs = require('node:fs');
+
+let data
+let result = 0
+
+try {
+  data = fs.readFileSync('data.txt', 'utf8').toString().toLowerCase().split("\n");
+} catch (err) {
+  console.error(err);
+}
+
+
+const dataObjects = data.reduce((cards, row) => {
+  // split the row '|'
+  const [cardPart, numbersPart] = row.split('|');
+
+  // Get card nr
+  const cardNumber = cardPart.split(':')[0].trim();
+
+  // Split string and convert to array of numbers
+  const winNumbers = cardPart.split(': ')[1].trim().split(' ').filter(value => value.length > 0).map(Number);
+  const yourNumbers = numbersPart.trim().split(' ').filter(value => value.length > 0).map(Number);
+
+  // Add to cards
+  cards[cardNumber] = { win: winNumbers, your: yourNumbers };
+
+  return cards;
+}, {}); // Start with an empty object
+
+
+console.log(result)
