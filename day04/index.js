1ce3e01932f327367cf5fde8a15c3bbee4d6e0d3
Tobias Andersson <HT86@live.se>
2023-12-04 11:41:22 +0100

    day 4 object created

--- day04/index.js
+++ day04/index.js
@@ -9,9 +9,28 @@ try {
   console.error(err);
 }
 
-data.forEach(dataRow => {
+const dataObjects = data.reduce((cards, row) => {
+  // split the row '|'
+  const [cardPart, numbersPart] = row.split('|');
 
-});
+  // Get card nr
+  const cardNumber = cardPart.split(':')[0].trim();
+
+  // Split string and convert to array of numbers
+  const winNumbers = cardPart.split(': ')[1].trim().split(' ').filter(value => value.length > 0).map(Number);
+  const yourNumbers = numbersPart.trim().split(' ').filter(value => value.length > 0).map(Number);
+
+  // Add to cards
+  cards[cardNumber] = { win: winNumbers, your: yourNumbers };
+
+  return cards;
+}, {}); // Start with an empty object
+
+console.log(JSON.stringify(dataObjects, null, 4))
+
+
+
+
 
 
 // Part One
