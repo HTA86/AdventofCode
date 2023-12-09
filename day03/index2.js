690e437b15bfdaff3793a121487974b63600b1ee
Tobias Andersson <HT86@live.se>
2023-12-09 16:25:53 +0100

    day3 part 2 finished

--- day03/index2.js
+++ day03/index2.js
@@ -5,57 +5,71 @@ let result = 0
 
 try {
   // Replace all symbols with X and split rows
-  data = fs.readFileSync('exemp.txt', 'utf8').toString().toLowerCase().replace(/\*/g, 'X').split('\n');
+  data = fs.readFileSync('data.txt', 'utf8').toString().toLowerCase().replaceAll(/\*/g, 'X').split('\n');
 } catch (err) {
   console.error(err);
 }
 
-
-
-// ------ ------ -------- ----- --- -- -
-// ------ ------ -------- ----- --- -- -
-// ------ ------ -------- ----- --- -- -
-// ------ ------ -------- ----- --- -- -
-// NOT
-//      STARTED
-//                Yet
-// ------ ------ -------- ----- --- -- -
-// ------ ------ -------- ----- --- -- -
-// ------ ------ -------- ----- --- -- -
-// ------ ------ -------- ----- --- -- -
-
-
-
-
 const dataArray = data.reduce((newRow, row) => {
-  // Split numbers
-  newRow.push(row.split(/(\d+)/g).filter(value => { return value.length > 0 }))
+  // Split X
+  newRow.push(row.split(/(X)/g).filter(value => { return value.length > 0 }))
   return newRow
 }, []); // Start with an empty array
 
+const findFullNumber = (str, index) => {
+  let [startIndex, endIndex] = [index, index];
+  const regexNr = /\d/
 
-// Check row
-const isValid = (nrStart, nrEnd, rowNr) => {
-  let includesX = false
-  const maxRowToCheck = rowNr+1 > data.length -1 ? data.length -1 : rowNr+1
-  const minRowToCheck = rowNr >  0 ? rowNr-1 : rowNr
+  // Finde the begining of the number
+  while (startIndex > 0 && regexNr.test(str[startIndex - 1])) { startIndex--; }
 
-  for (let rowToCheck = minRowToCheck; rowToCheck <= maxRowToCheck; rowToCheck++) {
-    //console.log('Row To Check = ' + rowToCheck + ' rowNr: ' + rowNr)
-    const rowPartToCheck = data[rowToCheck].substring(nrStart-1, nrEnd +2)
-    includesX = rowPartToCheck.toLowerCase().includes('x') ? true : includesX
-    //console.log(rowPartToCheck)
-  }
-  
-  // console.log('CHECK ROW')
-  
-  
-  
-  return includesX
+  // Finde the end of the number
+  while (endIndex < str.length && regexNr.test(str[endIndex])) { endIndex++; }
+
+  // Extract and return the number
+  return str.slice(startIndex, endIndex);
 }
 
+// Check row
+const isValid = (starIndex, rowNr) => {
+  //let includesX = false
+  const maxRowToCheck = rowNr + 1 > data.length - 1 ? data.length - 1 : rowNr + 1
+  const minRowToCheck = rowNr > 0 ? rowNr - 1 : rowNr
 
+  let numberAdjacentIndex = []
+  let numbersArray = []
 
+  for (let rowToCheck = minRowToCheck; rowToCheck <= maxRowToCheck; rowToCheck++) {
+    const rowPartToCheck = data[rowToCheck].substring(starIndex - 1, starIndex + 2)
+
+    // Regex match any number
+    const regex = /\d/
+    const matchNumber = rowPartToCheck.match(regex)
+
+    if (matchNumber) {
+      const [value1, value2, value3] = rowPartToCheck;
+      const value1Match = value1.match(regex)
+      const value3Match = value3.match(regex)
+
+      // 2 numbers
+      const twoNumbers = ( (value2.includes('.') || value2.includes('X')) && value1Match && value3Match) ? true : false
+      if (twoNumbers) {
+        let matchObject1 = { index: (starIndex - 1), row: rowToCheck }
+        let matchObject2 = { index: (starIndex + 1), row: rowToCheck }
+        numberAdjacentIndex.push(matchObject1)
+        numberAdjacentIndex.push(matchObject2)
+      } else {
+        let matchObject = { index: ((starIndex - 1) + matchNumber.index), row: rowToCheck }
+        numberAdjacentIndex.push(matchObject)
+      }
+    }
+  }
+  numberAdjacentIndex.forEach(adjecentIndex => {
+    numbersArray.push(findFullNumber(data[adjecentIndex.row], adjecentIndex.index))
+  });
+
+  return numbersArray
+}
 
 
 dataArray.forEach((row, rowNr) => {
@@ -63,33 +77,23 @@ dataArray.forEach((row, rowNr) => {
 
   row.forEach(value => {
     const indexStart = nextStart
-    const indexEnd = nextStart + ( value.length - 1)
-    const isNumber = Number(value) >= 0 || Number(value) <= 0
+    const isX = value == 'X'
 
-    //console.log('value: ' + value + ' indexStart: ' + indexStart + ' indexEnd: ' + indexEnd + ' rowNr: ' + rowNr)
-    if(isNumber) {
-      
-      let isAValidNumber = isValid(indexStart, indexEnd, rowNr)
-      if (isAValidNumber) {
-        //console.log(Number(value))
-        result += Number(value)
-      }
-      //result += isAValidNumber ? Number(value) : result
+    if (isX) {
+      let validXNumbers = isValid(indexStart, rowNr)
+      const validSum = validXNumbers.length == 2 ? ( validXNumbers[0] * validXNumbers[1] ) : 0
+      //console.log(validSum)
+      result += Number(validSum)
+
     }
     nextStart += value.length
   });
-
-  //console.log(row)
 });
 
 
-
-//console.log(dataArray)
-
-// Part One
-console.log(`Part One: ${result}`)
-
-
 // Part Two
-//result = 0
-//console.log(`Part Two: ${result}`)
+console.log(`Part Two: ${result}`)
+
+// 77926915 your answer is too low
+
+// 81939900 Correct
