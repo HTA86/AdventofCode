da1bf816db408bf5cc585451a03c1a968fa56da0
Tobias Andersson <HT86@live.se>
2023-12-09 11:18:42 +0100

    day3 solved

--- day03/index.js
+++ day03/index.js
@@ -4,118 +4,75 @@ let data
 let result = 0
 
 try {
-  data = fs.readFileSync('data.txt', 'utf8').toString().toLowerCase().split('\n');
+  // Replace all symbols with X and split rows
+  data = fs.readFileSync('data.txt', 'utf8').toString().toLowerCase().replace(/[*\-=$+&@#/%]/g, 'X').split('\n');
 } catch (err) {
   console.error(err);
 }
 
-const rowLength = data[0].length - 1
-const numberOfRows = data.length - 1
-let dataObject = {}
+const dataArray = data.reduce((newRow, row) => {
+  // Split numbers
+  newRow.push(row.split(/(\d+)/g).filter(value => { return value.length > 0 }))
+  return newRow
+}, []); // Start with an empty array
 
-data.forEach((dataRow, index) => {
-  const nrAndSymbol = dataRow.split('.').filter((a) => { return a.length > 0 })
 
-  dataObject[index] = { values: [], symbols: [] }
+// Check row
+const isValid = (nrStart, nrEnd, rowNr) => {
+  let includesX = false
+  const maxRowToCheck = rowNr+1 > data.length -1 ? data.length -1 : rowNr+1
+  const minRowToCheck = rowNr >  0 ? rowNr-1 : rowNr
 
-  nrAndSymbol.forEach(valueAndSymbol => {
-    // Only number
-    const valueSplit = valueAndSymbol.split(/[^a-zA-Z0-9]/).filter(a => { return a.length > 0 })[0]
-    const value = valueSplit ? valueSplit.replace(/[^0-9]/g, '') : undefined
-    let valuePlace = 0
+  for (let rowToCheck = minRowToCheck; rowToCheck <= maxRowToCheck; rowToCheck++) {
+    //console.log('Row To Check = ' + rowToCheck + ' rowNr: ' + rowNr)
+    const rowPartToCheck = data[rowToCheck].substring(nrStart-1, nrEnd +2)
+    includesX = rowPartToCheck.toLowerCase().includes('x') ? true : includesX
+    //console.log(rowPartToCheck)
+  }
+  
+  // console.log('CHECK ROW')
+  
+  
+  
+  return includesX
+}
 
-    const valueExists = dataObject[index].values.some(obj => obj.hasOwnProperty(value));
 
-    const regex = new RegExp(`\\b${value}\\b`, 'g');
-    let match;
-    let indexesValues = [];
-    while ((match = regex.exec(dataRow)) !== null) {
-      indexesValues.push(match.index);
-    }
 
-    if(indexesValues.length > 1 && valueExists) {
-      valuePlace = indexesValues[1]
-    } else {
-      valuePlace = indexesValues[0]
-    }    
 
-    const symbol = valueAndSymbol.split(/\d/g).filter(a => { return a.length > 0 })[0]
 
-    if (value) {
-      dataObject[index].values.push({ [value]: { place: valuePlace, included: false } })
-    }
+dataArray.forEach((row, rowNr) => {
+  let nextStart = 0
 
-    if (symbol) {
-      const allIndexes = []
+  row.forEach(value => {
+    const indexStart = nextStart
+    const indexEnd = nextStart + ( value.length - 1)
+    const isNumber = Number(value) >= 0 || Number(value) <= 0
 
-      for (let i = 0; i < dataRow.length; i++) {
-        if (dataRow[i] === symbol) {
-          allIndexes.push(i)
-        }
+    //console.log('value: ' + value + ' indexStart: ' + indexStart + ' indexEnd: ' + indexEnd + ' rowNr: ' + rowNr)
+    if(isNumber) {
+      
+      let isAValidNumber = isValid(indexStart, indexEnd, rowNr)
+      if (isAValidNumber) {
+        //console.log(Number(value))
+        result += Number(value)
       }
-
-      const symbolPlace = dataRow.indexOf(symbol)
-
-      const unicSet = new Set([...dataObject[index].symbols, ...allIndexes])
-
-      dataObject[index].symbols = [...unicSet]
+      //result += isAValidNumber ? Number(value) : result
     }
-  });
-});
-
-
-
-Object.keys(dataObject).forEach(rowIndex => {
-  const row = dataObject[rowIndex]
-
-  row.values.forEach((valueData, valueIndex) => {
-    const value = Object.keys(valueData)[0]
-    const valueStart = valueData[value].place
-    const valueEnd = valueStart + (value.length - 1)
-
-    const findValueStart = valueStart < 1 ? 0 : valueStart - 1
-    const findValueEnd = valueEnd == rowLength ? rowLength : valueEnd + 1
-
-    // Check if symbol
-    const rowsToCheck = [rowIndex]
-    if (Number(rowIndex) > 0) { rowsToCheck.push(Number((rowIndex) - 1).toString()) }
-    if (Number(rowIndex) < numberOfRows) { rowsToCheck.push((Number(rowIndex) + 1).toString()) }
-
-    rowsToCheck.forEach(checkRowIndex => {
-
-      const symbolIndexArray = dataObject[checkRowIndex].symbols
-
-      symbolIndexArray.forEach(symbolIndex => {
-
-        if (symbolIndex && symbolIndex >= findValueStart && symbolIndex <= findValueEnd) {
-          dataObject[rowIndex].values[valueIndex][value].included = true
-        }
-      });
-
-
-    });
-  })
-});
-
-console.log(JSON.stringify(dataObject, null, 4))
-
-
-Object.keys(dataObject).forEach(rowId => {
-  dataObject[rowId].values.forEach(valueObject => {
-    let values = Object.keys(valueObject).filter(valueKey => { return valueObject[valueKey].included })
-
-    values.forEach(value => {
-      result += Number(value)
-    });
+    nextStart += value.length
   });
 
+  //console.log(row)
 });
 
+
+
+//console.log(dataArray)
 
 // Part One
 console.log(`Part One: ${result}`)
 
 
 // Part Two
-result = 0
-console.log(`Part Two: ${result}`)
+//result = 0
+//console.log(`Part Two: ${result}`)
---
+++ day03/indexTryOne.js
@@ -0,0 +1,122 @@
+const fs = require('node:fs');
+
+let data
+let result = 0
+
+try {
+  data = fs.readFileSync('data.txt', 'utf8').toString().toLowerCase().split('\n');
+} catch (err) {
+  console.error(err);
+}
+
+const numberOfRows = data.length - 1
+let dataObject = {}
+
+data.forEach((dataRow, index) => {
+  const nrAndSymbol = dataRow.split('.').filter((a) => { return a.length > 0 })
+
+  dataObject[index] = { values: [], symbols: [] }
+
+  nrAndSymbol.forEach(valueAndSymbol => {
+    // Only number
+    const valueSplit = valueAndSymbol.split(/[^a-zA-Z0-9]/).filter(a => { return a.length > 0 })[0]
+    const value = valueSplit ? valueSplit.replace(/[^0-9]/g, '') : undefined
+    let valuePlace = 0
+
+    const valueExists = dataObject[index].values.some(obj => obj.hasOwnProperty(value));
+
+    const regex = new RegExp(`\\b${value}\\b`, 'g');
+    let match;
+    let indexesValues = [];
+    while ((match = regex.exec(dataRow)) !== null) {
+      indexesValues.push(match.index);
+    }
+
+    if(indexesValues.length > 1 && valueExists) {
+      valuePlace = indexesValues[1]
+    } else {
+      valuePlace = indexesValues[0]
+    }    
+
+    const symbol = valueAndSymbol.split(/\d/g).filter(a => { return a.length > 0 })[0]
+
+    if (value) {
+      dataObject[index].values.push({ [value]: { place: valuePlace, included: false } })
+    }
+
+    if (symbol) {
+      const allIndexes = []
+
+      for (let i = 0; i < dataRow.length; i++) {
+        if (dataRow[i] === symbol) {
+          allIndexes.push(i)
+        }
+      }
+
+      const symbolPlace = dataRow.indexOf(symbol)
+
+      const unicSet = new Set([...dataObject[index].symbols, ...allIndexes])
+
+      dataObject[index].symbols = [...unicSet]
+    }
+  });
+});
+
+
+
+Object.keys(dataObject).forEach(rowIndex => {
+  const row = dataObject[rowIndex]
+  const rowLength = dataObject[rowIndex].length - 1
+
+  row.values.forEach((valueData, valueIndex) => {
+    const value = Object.keys(valueData)[0]
+    const valueStart = valueData[value].place
+    const valueEnd = valueStart + (value.length - 1)
+
+    const findValueStart = valueStart < 1 ? 0 : valueStart - 1
+    const findValueEnd = valueEnd == rowLength ? rowLength : valueEnd + 1
+
+    // Check if symbol
+    const rowsToCheck = [rowIndex]
+    if (Number(rowIndex) > 0) { rowsToCheck.push(Number((rowIndex) - 1).toString()) }
+    if (Number(rowIndex) < numberOfRows) { rowsToCheck.push((Number(rowIndex) + 1).toString()) }
+
+    rowsToCheck.forEach(checkRowIndex => {
+
+      const symbolIndexArray = dataObject[checkRowIndex].symbols
+
+      symbolIndexArray.forEach(symbolIndex => {
+
+        if (symbolIndex && symbolIndex >= findValueStart && symbolIndex <= findValueEnd) {
+          dataObject[rowIndex].values[valueIndex][value].included = true
+        }
+      });
+
+
+    });
+  })
+});
+
+//console.log(JSON.stringify(dataObject, null, 4))
+
+
+Object.keys(dataObject).forEach(rowId => {
+  dataObject[rowId].values.forEach(valueObject => {
+    let values = Object.keys(valueObject).filter(valueKey => { return valueObject[valueKey].included })
+
+    values.forEach(value => {
+      result += Number(value)
+    });
+  });
+
+});
+
+
+// Part One
+console.log(`Part One: ${result}`)
+// fel: 528636
+
+
+// Part Two
+//result = 0
+//console.log(`Part Two: ${result}`)
