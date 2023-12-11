d14c2cde06ddd4dc1ee5bf2f4441fbb97510ca7e
Tobias Andersson <HT86@live.se>
2023-12-11 10:17:26 +0100

    day12 prep

---
+++ day12/index.js
@@ -0,0 +1,12 @@
+const fs = require('node:fs');
+
+let data
+let result = 0
+
+try {
+  data = fs.readFileSync('data.txt', 'utf8').toString().toLowerCase().split("\n");
+} catch (err) {
+  console.error(err);
+}
+
+console.log('result: ' + result)
