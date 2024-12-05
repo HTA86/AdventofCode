a403a19f1fc305475542aad556d24b89b1a7be0e
Tobias Andersson <HT86@live.se>
2024-12-05 15:48:43 +0100

    part2 bad start

--- 2024/day05/index.js
+++ 2024/day05/index.js
@@ -5,7 +5,7 @@ let result = 0
 
 
 try {
-  data = fs.readFileSync('data.txt', 'utf8').toString().toLowerCase().split("\n");
+  data = fs.readFileSync('sample2.txt', 'utf8').toString().toLowerCase().split("\n");
 } catch (err) {
   console.error(err);
 }
@@ -14,10 +14,8 @@ try {
 const splitIndex = data.indexOf('');
 let orderingRules = data.slice(0, splitIndex);
 let pageNumbers = data.slice(splitIndex + 1);
+let wrongPageNumbers = [];
 
-
-//console.log(orderingRules)
-//console.log(pageNumbers)
 
 const pageFolowTheRules = (arr) => {
   let pagePass = true;
@@ -25,7 +23,6 @@ const pageFolowTheRules = (arr) => {
 
   while (pagePass) {
     let currentNr = parseInt(arr[i]);
-    //console.log(currentNr);
 
     // filter the rules
     let rules = orderingRules.filter(rule => {
@@ -34,36 +31,84 @@ const pageFolowTheRules = (arr) => {
     })
 
     let orderPass = true;
-    let j = i+1;
+    let j = i + 1;
 
     while (orderPass && arr.length > j) {
-      let currentOrder = currentNr + '|' + arr[j];
       let reverseOrder = arr[j] + '|' + currentNr;
-      
-      //console.log(currentOrder)
-      //let correctOrder = rules.filter(rule => { return rule == currentOrder }).length > 0;
       let incorrectOrder = rules.filter(rule => { return rule == reverseOrder }).length > 0;
 
-      if(incorrectOrder) { orderPass = false }
-
-      //console.log('correctOrder: ' + correctOrder)
-      //console.log('incorrectOrder: ' + incorrectOrder)
+      if (incorrectOrder) { orderPass = false }
       j++
     }
 
     pagePass = orderPass;
 
-    //console.log(rules)
-    //console.log('')
-
-
     i++ // brake if array end
     if (i == arr.length) { break }
-
   }
-
-
   return pagePass
+}
+
+
+const checkValidOrder = (nrOne, nrTwo) => {
+  // filter the rules
+  return orderingRules.filter(rule => {
+    let [left, right] = rule.split('|');
+    return left == nrOne && right == nrTwo;
+  }).length > 0;
+}
+
+const correctTheOrder = (arr) => {
+  console.log(arr)
+  let incorrectOrder = true
+  let i = 0
+  let newArray = [...arr]
+
+  while (incorrectOrder) {
+    let currentNr = arr[i]
+    let nextNr = arr[i + 1]
+    console.log(currentNr + ' ' + nextNr)
+
+    // filter the rules
+    let isValidOrder = checkValidOrder(currentNr, nextNr)
+
+    // If incorrect order, move the current nr 1 forward
+    if (!isValidOrder) {
+      let iToMove = i
+      let moveTo = i + 1
+      let maxMove = newArray.length-1
+      while (maxMove > 0) {
+        console.log('Move ' + iToMove + ' forward')
+        // Number to move
+        let numberToMove = newArray.splice(iToMove, 1)[0];
+        // insert one forword
+        newArray.splice(moveTo, 0, numberToMove);
+        console.log(newArray)
+        maxMove--
+
+        // If the moveTo is moved to the start, set iToMove to 0
+        if (moveTo === 0) {
+          iToMove = 0
+        } else {
+          iToMove++
+        }
+
+        // If the moved number is in the last place, place it in beginig of the array
+        if (moveTo === newArray.length - 1) {
+          moveTo = 0
+        } else {
+          moveTo++
+        }
+
+
+
+      }
+    }
+    console.log(isValidOrder)
+
+    i++
+    incorrectOrder = i < (arr.length - 1)
+  }
 }
 
 
@@ -73,26 +118,32 @@ console.time('Execution Time Part One');
 pageNumbers.forEach(nrRow => {
   let nrArray = nrRow.split(',')
   let midIndex = Math.floor(nrArray.length / 2);
+  let midNr = 0
 
-  let midNr = pageFolowTheRules(nrArray) ? Number(nrArray[midIndex]) : 0;
+
+
+  if (pageFolowTheRules(nrArray)) {
+    midNr = Number(nrArray[midIndex])
+  } else {
+    wrongPageNumbers.push(nrRow)
+  }
 
   result += midNr
-  //console.log(test)
-
-
-
 });
-
-
 console.timeEnd('Execution Time Part One');
 console.log('Part one: ' + result)
 
 
+
 // Part Two
-result = 0
+result = 0;
 console.time('Execution Time Part Two');
 
-
+wrongPageNumbers.forEach(nrRow => {
+  let nrArray = nrRow.split(',')
+  // Correct the order
+  correctTheOrder(nrArray)
+});
 
 console.timeEnd('Execution Time Part Two');
 console.log('Part two: ' + result)
